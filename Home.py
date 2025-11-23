77bdabf4195536e6f44514aeab239bcb2e5a0e9f
Mohamed <mohamed@Mohameds-MacBook-Air.local>
2025-11-24 00:44:10 +0400

    implemented task 1,2,3

--- Home.py
+++ Home.py
@@ -1,9 +1,7 @@
 import streamlit as st
+from app.data import df
 st.title("Hello, S")
 
 
 st.set_page_config()
 st.text_input("login")
-
-if st.button("Analytics"):
-    st.switch_page("pages/Analytics")
