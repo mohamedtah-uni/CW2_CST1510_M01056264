6ee0e5ccb30c26ed185f8152ecf8cde714df8201
Mohamed Tah <greyhathacker092@gmail.com>
2025-12-11 21:08:19 +0400

    Revert "week 8"
    This reverts commit ca2700f8a65399965903d795807db96ed2c02431.

--- pages/Analytics.py
+++ pages/Analytics.py
@@ -1,10 +1,5 @@
 
 import streamlit as st
 
-def authenticate():
-    if not st.session_state.get("logged_in", False):
-        st.switch_page("Home.py")
 
-authenticate()
-
-st.title("Analytics Page")
+st.title("Hello, S")
--- pages/Dashboard.py
+++ pages/Dashboard.py
@@ -1,16 +1,2 @@
+
 import streamlit as st
-
-
-def authenticate():
-    if not st.session_state.get("logged_in", False):
-        st.switch_page("Home.py")
-
-if "logged_in" not in st.session_state:
-    st.session_state.logged_in = False
-
-authenticate()
-
-st.title("Dashboard")
-
-# navigate to role based dashboard
-# or render dashboard according to user's role
